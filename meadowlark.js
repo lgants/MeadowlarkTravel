a584172c750b11f993db7159afa1bb6d3a3838fe
lgants <lgants@gmail.com>
2016-12-29 23:49:55 -0600

    added formidable package

--- meadowlark.js
+++ meadowlark.js
@@ -2,7 +2,7 @@ var path = require('path');
 var express = require('express');
 var handlebars = require('express3-handlebars');
 var fortune = require('./lib/fortune.js');
-
+var formidable = require('formidable');
 var app = express();
 
 // set up handlebars view engine
@@ -19,12 +19,17 @@ app.engine('handlebars', handlebars({
   }
 }));
 app.set('view engine', 'handlebars');
-
-
 app.set('port', process.env.PORT || 3000);
 
 // static middleware for files to serve client
 app.use(express.static(__dirname + '/public'));
+
+// middleware to parse URL-encoded body of POST requests
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // if test=1 appears in the querystring for any page the property res.locals.showTests will be set to true
 app.use(function(req, res, next) {
@@ -55,15 +60,41 @@ app.get('/tours/request-group-rate', function(req, res){
   res.render('tours/request-group-rate');
 });
 
-app.get('/nursery-rhyme', function(req, res){
-  res.render('nursery-rhyme');
+app.get('/newsletter', function(req, res){
+  res.render('newsletter', { csrf: 'CSRF token goes here' });
 });
-app.get('/data/nursery-rhyme', function(req, res){
-  res.json({
-    animal: 'squirrel',
-    bodyPart: 'tail',
-    adjective: 'bushy',
-    noun: 'heck',
+app.post('/process', function(req, res){
+  console.log('Form (from querystring): ' + req.query.form);
+  console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+  console.log('Name (from visible form field): ' + req.body.name);
+  console.log('Email (from visible form field): ' + req.body.email);
+  res.redirect(303, '/thank-you');
+});
+
+app.post('/process', function(req, res){
+  if(req.xhr || req.accepts('json,html')==='json'){
+  // if there were an error, we would send { error: 'error description' }
+  res.send({ success: true }); }else{
+    // if there were an error, we would redirect to an error page
+    res.redirect(303, '/thank-you');
+  }
+});
+
+app.get('/contest/vacation-photo',function(req,res){
+  var now = new Date();
+  res.render('contest/vacation-photo',{
+    year: now.getFullYear(),month: now.getMont()
+  });
+});
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files){
+    if(err) return res.redirect(303, '/error');
+    console.log('received fields:');
+    console.log(fields);
+    console.log('received files:');
+    console.log(files);
+    res.redirect(303, '/thank-you');
   });
 });
 
